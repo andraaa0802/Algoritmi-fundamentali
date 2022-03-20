a5dfe8d3c6e81345c4794364331723c848e8a8b6
Cristina-Andrada Vinter <vinter.cristinaandrada@student.uoradea.ro>
2022-03-20 22:51:31 +0200

    proba

---
+++ 21Martie/21Martie/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _21Martie
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
