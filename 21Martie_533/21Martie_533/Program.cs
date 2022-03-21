c9bcd2cc63afa89452bf4bf5f9f0a139e0274456
Cristina-Andrada Vinter <vinter.cristinaandrada@student.uoradea.ro>
2022-03-21 11:05:27 +0200

    pbinfo

---
+++ 21Martie_533/21Martie_533/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _21Martie_533
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int imin=-1, imax=-1, n, i, minim, maxim;
+            n=int.Parse(Console.ReadLine());   
+            string[]data=Console.ReadLine().Split(' ');
+            int[] v = new int[n];
+            for(i=0;i<n;i++)
+                v[i]=int.Parse(data[i]);
+            minim = v[0];
+            maxim = v[0];
+            for(i=1;i<n;i++)
+            {
+                if(v[i]<minim)
+                {
+                    minim = v[i];
+                    imin = i;
+                }
+                if (v[i]>maxim)
+                {
+                    maxim = v[i];
+                    imax = i;
+                }
+            }
+            Console.WriteLine((imin+1) + " " + (imax+1));
+        }
+    }
+}
