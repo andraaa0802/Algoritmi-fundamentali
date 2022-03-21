6d8055a7af0f30cb58ae46352fc1f33a90544631
Cristina-Andrada Vinter <vinter.cristinaandrada@student.uoradea.ro>
2022-03-21 10:39:25 +0200

    pbinfo

---
+++ 21Martie_488/21Martie_488/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _21Martie_488
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] v;
+            int n, i;
+            n = int.Parse(Console.ReadLine());
+            v=new int[n];
+            string[] data = Console.ReadLine().Split(' ');
+            for(i=0;i<n;i++)
+                v[i]=int.Parse(data[i]);
+            for (i = 1; i < n; i += 2)
+                Console.Write(v[i] + " ");
+            Console.WriteLine();
+            if (n % 2 == 0)
+                for (i = n - 2; i >= 0; i -= 2)
+                    Console.Write(v[i] + " ");
+            else
+                for (i = n - 1; i >= 0; i -= 2)
+                    Console.Write(v[i] + " ");
+
+        }
+    }
+}
