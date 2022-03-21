84767ad8aa703477ae75630843255c2084af334c
Cristina-Andrada Vinter <vinter.cristinaandrada@student.uoradea.ro>
2022-03-21 11:47:53 +0200

    pbinfo

---
+++ 492/492/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _492
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n,perechi=0;
+            n=int.Parse(Console.ReadLine());
+            int []v=new int[n];
+            string[] data = Console.ReadLine().Split(' ');
+            for (int i = 0; i < n; i++)
+                v[i] = int.Parse(data[i]);
+            for (int i=0;i<n/2;i++)
+                if (cmmdc(v[i],v[n-i-1])==1)
+                    perechi++;
+            Console.WriteLine(perechi);
+        }
+
+        static int cmmdc (int a, int b)
+        {
+            while (a != b)
+            {
+                if (a > b)
+                    a -= b;
+                if (b > a)
+                    b -= a;
+            }
+            return a;   
+        }
+    }
+}
